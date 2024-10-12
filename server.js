a3b4cdd1ce6ec72effd64cc65dffb80870ecddf4
revathyrajeev <revuparu8@gmail.com>
2024-10-13 00:25:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8080; 
 
 // Middleware
 app.use(express.static('public'));
